10b989d3355dda906af8a05eb99006f6e0c29909
Mrcan_dong <414541340@qq.com>
2025-05-12 15:58:30 +0800

    first

---
+++ rslidar_driver/rs_driver/demo/build/CMakeFiles/demo_online.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo_online.dir/demo_online.cpp.o"
+  "CMakeFiles/demo_online.dir/demo_online.cpp.o.d"
+  "demo_online"
+  "demo_online.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo_online.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
